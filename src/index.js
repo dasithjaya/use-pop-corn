f9ed2b1152294707d4a7fc92efdce66b67538094
DasithJ <dasith.jayanandana@ebuilder.com>
2024-03-06 21:35:24 +0530

    Added start rating reusable component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const[movieRating, setMovieRating] = useState(0)
+
+  return(
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating}/>
+      <p>This film rated {movieRating}</p>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amaizing"]}
+    />
+    <StarRating size={30} color="red" className="test" defaultRating={3}/>
+    <Test/>
   </React.StrictMode>
 );
-
